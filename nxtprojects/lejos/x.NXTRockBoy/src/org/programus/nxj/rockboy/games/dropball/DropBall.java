fe1a27003db34f3ded897b7b38ba913b72540d5f
Programus <Programus@23678b7a-ab25-11de-8ba5-91705c1537ed>
2009-12-12 15:58:57 +0000

    Volume setting modified. Add Transformers Theme BGM.

--- nxtprojects/lejos/x.NXTRockBoy/src/org/programus/nxj/rockboy/games/dropball/DropBall.java
+++ nxtprojects/lejos/x.NXTRockBoy/src/org/programus/nxj/rockboy/games/dropball/DropBall.java
@@ -2,6 +2,8 @@ package org.programus.nxj.rockboy.games.dropball;
 
 import java.io.IOException;
 
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
 import lejos.nxt.Sound;
 import lejos.util.TextMenu;
@@ -21,7 +23,7 @@ public class DropBall {
 //		RConsole.openUSB(15000); 
 		DropBallGame game = new DropBallGame(); 
 		game.initialize(); 
-		initMusics(); 
+		initMusicsAndSound(); 
 		try {
 			int overRow = 3; 
 			TextMenu againMenu = new TextMenu(new String[]{"YES", "NO"}, 6, "Play Again?"); 
@@ -90,13 +92,49 @@ public class DropBall {
 			"4E/16/32 40/32 4C/16/32 40/32 4D/16/32 40/32 4D/8 ", 
 			"4C/8 4E/8 40/16 4D/16 4C/16 3A/8 40/8", 
 		}), 
+		
+		// Transformers Theme
+		new Music(new SheetParam(SheetParam.NORMAL_BEAT_DUR, 1, new int[] {10, 0, 20, 0}, false), new String[] {
+			"30/4 ", 
+			"3E/8 3A/4 3B/8 4C/1 3A/4 3B/8 4C/4 3F/8 3F/1 ", 
+			"30/8 ", 
+			"3A/8 3B/8 4C/4 4C/4 4D/4 4E/4 4F/8 4D/4 4C/8 4D/4 4E/8 4C/4 3B/8 4C/4 4D/8 3B/4/8 3A/8 3G#/8 3B/8 3A/8 3A/4/8 ", 
+			"4E/8 4A/4 4B/8 5C/1 4A/4 4B/8 5C/4 4F/8 4F/1 ", 
+			"30/8 ", 
+			"4A/8 4B/8 5C/4 5C/4 5D/4 5E/4 5F/8 5D/4 5C/8 5D/4 5E/8 5C/4 4B/8 5C/4 5D/8 4B/4/8 4A/8 4G#/8 4B/8 4A/8 4A/4/8 ", 
+			"3E/8 3A/4 3B/8 4C/1 3A/4 3B/8 4C/4 3F/8 3F/1 ", 
+			"4E/8 4A/4 4B/8 5C/1 4A/4 4B/8 5C/4 4F/8 4F/1 ", 
+			"3E/8 3A/4 3B/8 4C/1 ", 
+			"4A/8 4B/8 4A/8 4G#/8 4A/2/4 40/4", 
+		}), 
 	}; 
 	
-	private static void initMusics() {
+	private static void initMusicsAndSound() {
+		Sound.setVolume(60); 
 		BGMBox box = BGMBox.getInstance(); 
-		box.setVol(50); 
+		box.setVolRate(50); 
 		for (Music music : musics) {
 			box.addMusic(music); 
 		}
+
+		final int VOL_UNIT = 5; 
+		Button.LEFT.addButtonListener(new ButtonListener(){
+			@Override
+			public void buttonPressed(Button b) {
+				Sound.setVolume(Sound.getVolume() - VOL_UNIT); 
+			}
+			@Override
+			public void buttonReleased(Button b) {
+			}
+		}); 
+		Button.RIGHT.addButtonListener(new ButtonListener(){
+			@Override
+			public void buttonPressed(Button b) {
+				Sound.setVolume(Sound.getVolume() + VOL_UNIT); 
+			}
+			@Override
+			public void buttonReleased(Button b) {
+			}
+		}); 
 	}
 }
