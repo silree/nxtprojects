17878c99e5c1264af1bf2738f3544f6843f4b1ad
programus <programus@23678b7a-ab25-11de-8ba5-91705c1537ed>
2011-01-24 16:32:12 +0000

    Mostly run OK. Not find big bug. :-)

--- trunk/nxtprojects/lejos/x.GuessColor/src/org/programus/nxj/guessnumber/GuessColor.java
+++ trunk/nxtprojects/lejos/x.GuessColor/src/org/programus/nxj/guessnumber/GuessColor.java
@@ -1,10 +1,17 @@
 package org.programus.nxj.guessnumber;
 
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
+import lejos.nxt.Sound;
 import lejos.util.TextMenu;
 
 import org.programus.nxj.guessnumber.game.GuessGame;
+import org.programus.nxj.util.DisplayUtil;
+import org.programus.nxj.util.txtimg.TextImage;
+import org.programus.nxj.util.txtimg.TextImage3x5;
 
 public class GuessColor {
 
@@ -12,14 +19,31 @@ public class GuessColor {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Sound.setVolume(Sound.VOL_MAX); 
+		displayDescription();
+		LCD.refresh(); 
+		Button.waitForPress(); 
 		GuessGame game = new GuessGame(); 
 		TextMenu againMenu = new TextMenu(new String[]{"YES", "NO"}, 6, "Play Again?"); 
 		do {
 			// play game again and again until the player select no in play again menu. 
 			game.playOneGame();
-			Button.ENTER.waitForPressAndRelease(); 
+			Button.ESCAPE.waitForPressAndRelease(); 
 			LCD.clear(); 
 		} while (againMenu.select() == 0); 
-
+	}
+	
+	private static void displayDescription() {
+		Graphics g = new Graphics(); 
+		g.clear(); 
+		g.fillRect(0, LCD.CELL_HEIGHT + (LCD.CELL_HEIGHT >> 1), g.getWidth(), LCD.CELL_HEIGHT << 1); 
+		DisplayUtil.drawStringCenter("GUESS COLOR", LCD.CELL_HEIGHT << 1, true); 
+		TextImage ti = new TextImage3x5(); 
+		Image pressAnyKey = ti.getImage("PRESS ANY KEY"); 
+		Image toStart = ti.getImage("TO START..."); 
+		int y = LCD.SCREEN_HEIGHT - (LCD.CELL_HEIGHT << 1); 
+		DisplayUtil.drawImageCenter(pressAnyKey, y, LCD.ROP_COPY); 
+		y += LCD.CELL_HEIGHT; 
+		DisplayUtil.drawImageCenter(toStart, y, LCD.ROP_COPY); 
 	}
 }
--- trunk/nxtprojects/lejos/x.GuessColor/src/org/programus/nxj/guessnumber/utils/ColorCombination.java
+++ trunk/nxtprojects/lejos/x.GuessColor/src/org/programus/nxj/guessnumber/utils/ColorCombination.java
@@ -2,25 +2,41 @@ package org.programus.nxj.guessnumber.utils;
 
 import java.util.Random;
 
+import lejos.robotics.Colors;
+
 public class ColorCombination {
 	public static final int COLOR_NUM = 4;
 	
-	private int[] colors; 
+	public static final int[] COLOR_TYPES = {Colors.RED, Colors.YELLOW, Colors.GREEN, Colors.BLUE}; 
+	public static final String[] COLOR_TYPE_DESCS = {"R", "Y", "G", "B", "X"}; 
+
+	private int[] colorIndices; 
 	public ColorCombination(int[] colors) {
-		this.colors = colors;
-	}
-	
-	public static ColorCombination getRandomCombination(int[] colorTypes, int countLimit) {
-		int[] allColors = new int[colorTypes.length * countLimit]; 
-		for (int i = 0; i < countLimit; i++) {
-			System.arraycopy(colorTypes, 0, allColors, i * colorTypes.length, colorTypes.length); 
+		colorIndices = new int[colors.length]; 
+		for (int i = 0; i < colors.length; i++) {
+			colorIndices[i] = -1;
+			for (int j = 0; j < COLOR_TYPES.length; j++) {
+				if (colors[i] == COLOR_TYPES[j]) {
+					colorIndices[i] = j;
+				}
+			}
+			if (colorIndices[i] < 0) {
+				colorIndices[i] = COLOR_TYPES.length;
+			}
 		}
-		return getRandomCombination(allColors); 
 	}
 	
-	public static ColorCombination getRandomCombination(int[] allAvailableColors) {
+	public static ColorCombination getRandomCombination(int countLimit, boolean allowBlank) {
+		return getRandomCombination(allowBlank ? COLOR_TYPES.length + 1 : COLOR_TYPES.length, countLimit); 
+	}
+	
+	public static ColorCombination getRandomCombination(int colorTypeNumber, int countLimit) {
+		int[] allAvailableColors = new int[colorTypeNumber * countLimit]; 
+		for (int i = 0; i < countLimit; i++) {
+			System.arraycopy(COLOR_TYPES, 0, allAvailableColors, i * colorTypeNumber, COLOR_TYPES.length); 
+		}
 		Random r = new Random(); 
-		for (int i = allAvailableColors.length; i > 1; i--) {
+		for (int i = allAvailableColors.length; i > allAvailableColors.length - COLOR_NUM; i--) {
 			int ii = i - 1;
 			int jj = r.nextInt(i); 
 			int tmp = allAvailableColors[ii]; 
@@ -28,17 +44,17 @@ public class ColorCombination {
 			allAvailableColors[jj] = tmp;
 		}
 		int[] colors = new int[COLOR_NUM]; 
-		System.arraycopy(allAvailableColors, 0, colors, 0, COLOR_NUM); 
+		System.arraycopy(allAvailableColors, allAvailableColors.length - COLOR_NUM - 1, colors, 0, COLOR_NUM); 
 		return new ColorCombination(colors); 
 	}
 	
 	public CompareResult compareTo(ColorCombination cc) {
 		CompareResult r = new CompareResult(); 
-		for (int i = 0; i < this.colors.length; i++) {
+		for (int i = 0; i < this.colorIndices.length; i++) {
 			boolean exact = false;
 			boolean exist = false;
-			for (int j = 0; j < cc.colors.length; j++) {
-				if (this.colors[i] == cc.colors[j]) {
+			for (int j = 0; j < cc.colorIndices.length; j++) {
+				if (this.colorIndices[i] == cc.colorIndices[j]) {
 					if (i == j) {
 						exact = true; 
 					} else {
@@ -59,8 +75,8 @@ public class ColorCombination {
 	public String toString() {
 		StringBuffer sb = new StringBuffer(); 
 		sb.append('['); 
-		for (int i = 0; i < this.colors.length; i++) {
-			sb.append(this.colors[i]); 
+		for (int i = 0; i < this.colorIndices.length; i++) {
+			sb.append(this.colorIndices[i]); 
 		}
 		sb.append(']'); 
 		return sb.toString(); 
@@ -69,14 +85,8 @@ public class ColorCombination {
 	public String toLcdString() {
 		StringBuffer sb = new StringBuffer(); 
 		sb.append('['); 
-		for (int i = 0; i < this.colors.length; i++) {
-			int index = 0;
-			for (int j = 0; j < Constants.COLOR_TYPES.length; j++) {
-				if (Constants.COLOR_TYPES[j] == this.colors[i]) {
-					index = j; 
-				}
-			}
-			sb.append(Constants.COLOR_TYPE_DESCS[index]); 
+		for (int i = 0; i < this.colorIndices.length; i++) {
+			sb.append(COLOR_TYPE_DESCS[this.colorIndices[i]]); 
 		}
 		sb.append(']'); 
 		return sb.toString(); 
@@ -94,8 +104,8 @@ public class ColorCombination {
 	@Override
 	public int hashCode() {
 		int ret = 0; 
-		for (int i = 0; i < this.colors.length; i++) {
-			ret = (ret << 1) + this.colors[i]; 
+		for (int i = 0; i < this.colorIndices.length; i++) {
+			ret = (ret << 1) + this.colorIndices[i]; 
 		}
 		return ret; 
 	}
